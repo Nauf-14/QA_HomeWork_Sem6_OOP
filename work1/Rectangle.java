989bb650d79b5defcf088a2eaf6e2fea7f61d770
Naufal <nauf1207@mail.ru>
2024-02-24 15:57:25 +0600

    решение work1

--- work1/Rectangle.java
+++ work1/Rectangle.java
@@ -1,9 +1,15 @@
 package QA_HomeWork_Sem6_OOP.work1;
 
-// Прямоугольник переопределил как наследника от Фигуры и логика получения площади
+/**
+ * Класс Rectangle представляет собой подкласс GeometricFigure и является представлением прямоугольника.
+ * Здесь определена дополнительная переменная a для хранения площади прямоугольника.
+ */
+
 public class Rectangle extends GeometricFigure{
 
-    int a;
+    int a; // Площадь прямоугольника
+
+    // Метод setArea() вычисляет площадь прямоугольника на основе заданных высоты и ширины.
     public void setArea(int height, int width) {
         a = height*width;
     }
